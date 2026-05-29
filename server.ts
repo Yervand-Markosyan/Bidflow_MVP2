3b03cc45e746f12127350c0a5ad9ec855771c365
Yervand-Markosyan <50021662+Yervand-Markosyan@users.noreply.github.com>
2026-05-30 03:08:54 +0400

    Fix Express 5 wildcard and db test

--- server.ts
+++ server.ts
@@ -623,7 +623,7 @@ async function startServer() {
   } else {
     const distPath = path.join(APP_ROOT, 'dist');
     app.use(express.static(distPath));
-    app.get('*all', (req, res) => {
+    app.get('/:any*', (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
     });
   }
