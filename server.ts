e6aaf360e631db7ab710d51371dc0d5f73c97e7e
Yervand-Markosyan <50021662+Yervand-Markosyan@users.noreply.github.com>
2026-05-30 03:19:47 +0400

    Deploy static content to Pages

--- server.ts
+++ server.ts
@@ -623,7 +623,7 @@ async function startServer() {
   } else {
     const distPath = path.join(APP_ROOT, 'dist');
     app.use(express.static(distPath));
-    app.get('/:any*', (req, res) => {
+    app.get(/^\/(?!api).*/, (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
     });
   }
